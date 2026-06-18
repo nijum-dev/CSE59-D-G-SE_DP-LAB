87562a07eea89f1015c4e336ecfef1b78d26406d
Khatune Jannat Nijum <jannatnijum.dev@gmail.com>
2026-06-18 15:58:31 +0600

    Add Main1 class to print the first score

--- Task 2/Main1.java
+++ Task 2/Main1.java
@@ -1,1 +1,15 @@
 
+class Main1 {
+    int[] scores = {85, 90, 95};
+
+    void printFirstScore() {
+        System.out.println("First Score: " + scores[0]);
+    }
+}
+
+public class ContestStats {
+    public static void main(String[] args) {
+        Main1 contest = new Main1();
+        contest.printFirstScore();
+    }
+}
